878259b6dfb551ba9e01b0d56521262cb801c0bc
Marek 'marx' Grác <mgrac@redhat.com>
2017-04-11 13:30:33 +0200

    [feat] scene: crypto game page

--- index.ios.js
+++ index.ios.js
@@ -4,11 +4,12 @@ import { AppRegistry } from 'react-native';
 import AboutScene from './scenes/aboutScene';
 import VisitorsScene from './scenes/visitorsScene';
 import ServicesScene from './scenes/servicesScene';
+import GameScene from './scenes/gameScene';
 
 export default class zobro2 extends Component {
   render() {
     return (
-      <ServicesScene />
+      <GameScene />
     );
   }
 }
