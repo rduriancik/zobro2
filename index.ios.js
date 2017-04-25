b16299335c8c7635e626a0a2edd0545f9ed0f121
Marek 'marx' Grác <mgrac@redhat.com>
2017-04-25 15:45:38 +0200

    [feat] scene: Main Menu with working navigation

--- index.ios.js
+++ index.ios.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 
+import { StackNavigator } from 'react-navigation';
+
 import AboutScene from './scenes/aboutScene';
 import VisitorsScene from './scenes/visitorsScene';
 import ServicesScene from './scenes/servicesScene';
 import GameScene from './scenes/gameScene';
 import EventsScene from './scenes/eventsScene';
+import MenuScene from './scenes/menuScene';
 
-export default class zobro2 extends Component {
-  render() {
-    return (
-      <EventsScene />
-    );
-  }
-}
+//import * as scenes from './scenes.js';
+
+const zobro2 = StackNavigator({
+  MAIN_MENU: { screen: MenuScene },
+  ABOUT: { screen: AboutScene },
+  EVENTS: { screen: EventsScene },
+  SERVICES: { screen: ServicesScene },
+  GAME: { screen: GameScene },
+  VISITORS: { screen: VisitorsScene },
+}, {
+  initialRouteName: 'MAIN_MENU',
+});
 
 AppRegistry.registerComponent('zobro2', () => zobro2);
---
+++ scenes/menuScene.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import styles from '../components/styles';
+
+import {
+  ScrollView,
+  TouchableHighlight,
+  View,
+  Alert,
+} from 'react-native';
+import Text from '../components/text';
+
+import * as scenes from '../scenes';
+
+class MainMenuItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const bgColor = scenes.sceneTitles[this.props.scene].bgColor;
+    const title = scenes.sceneTitles[this.props.scene].title;
+
+    return (
+      <TouchableHighlight
+        underlayColor='#aaaaaa'
+        onPress={() => {this.props.navigation.navigate(scenes.sceneTitles[this.props.scene].name)}}
+        >
+          <View style={[styles.mainMenuItem, {backgroundColor: bgColor}]}>
+            <Text style={[styles.mainMenuItemText, {
+              textAlign: this.props.alignText,
+              paddingRight: 32,
+              paddingLeft: 32,
+            }]}>{title}</Text>
+          </View>
+        </TouchableHighlight>
+    );
+  }
+}
+
+export default class MenuScene extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const MIN_ZOOM = 0.5;
+    const MAX_ZOOM = 5.0;
+    const TEXT_COLOR = 'black';
+
+    const PADDING = 20;
+    const menuItems = [scenes.EVENTS, scenes.GAME, scenes.SERVICES, scenes.VISITORS, scenes.ABOUT];
+
+    return (
+      <ScrollView minimumZoomScale={MIN_ZOOM} maximumZoomScale={MAX_ZOOM} style={styles.contentView}>
+        {
+        menuItems.map((itemInMenu, index) => (
+          <MainMenuItem
+            key={index}
+            alignText={index % 2 ? 'left' : 'right'}
+            scene={itemInMenu}
+            navigation={this.props.navigation}
+          />
+        ))
+        }
+      </ScrollView>
+    );
+  }
+}
