5d14fb0ad13fd4cec7912e800c75ff8db7851549
Marek 'marx' Grác <mgrac@redhat.com>
2017-05-09 15:16:08 +0200

    [feat] Add clickable content GPS/phone

--- scenes/visitorsScene.js
+++ scenes/visitorsScene.js
@@ -5,10 +5,13 @@ import {
   View,
   ScrollView,
   Image,
+  Linking,
+  TouchableHighlight,
 } from 'react-native';
 import Text from '../components/animalText';
 import ImageLabel from '../components/imageLabel';
 import Dimensions from 'Dimensions';
+import Hyperlink from 'react-native-hyperlink'
 
 export default class VisitorsScene extends React.Component {
   constructor(props) {
@@ -17,6 +20,10 @@ export default class VisitorsScene extends React.Component {
 
   static navigationOptions = {
     title: 'Pro návštěvníky',
+  }
+
+  _openURL(url) {
+    Linking.openURL(url);
   }
 
   render() {
@@ -31,9 +38,9 @@ export default class VisitorsScene extends React.Component {
     const IMAGE_STYLE = {width: ICON_SIZE, height: ICON_SIZE};
 
     const data = [
-      {source: require('../images/icons/telefon.png'), text: '546 432 311'},
-      {source: require('../images/icons/fax.png'), text: '546 210 000'},
-      {source: require('../images/icons/gps.png'), text: '49° 13\' 49,6\'\' N\n16° 31\' 59,9\'\' E'},
+      {source: require('../images/icons/telefon.png'), text: '546 432 311', url: 'tel://546432311'},
+      {source: require('../images/icons/fax.png'), text: '546 210 000', url: 'tel://546210000'},
+      {source: require('../images/icons/gps.png'), text: '49° 13\' 49,6\'\' N\n16° 31\' 59,9\'\' E', url: 'http://maps.apple.com/?daddr=49.2304444,16.533305555555554'},
       {source: require('../images/icons/tramvaj.png'), text: 'č. 1, 3, 11'},
       {source: require('../images/icons/autobus.png'), text: 'č. 50, 52, 54'},
       {source: require('../images/icons/trolejbus.png'), text: 'č. 30'},
@@ -42,11 +49,21 @@ export default class VisitorsScene extends React.Component {
 
     let renderedData = [];
     data.forEach((element, idx) => {
-      renderedData.push(
-        <ImageLabel key={idx} source={element.source} style={IMAGE_STYLE}>
-          <Text style={[{color: TEXT_COLOR, textAlignVertical: 'center', paddingLeft: 10}]}>{element.text}</Text>
-        </ImageLabel>
-      )
+      if (element.hasOwnProperty('url')) {
+        renderedData.push(
+          <ImageLabel key={idx} source={element.source} style={IMAGE_STYLE}>
+            <TouchableHighlight onPress={() => this._openURL(element.url)}>
+              <Text style={[{color: 'blue', textAlignVertical: 'center', paddingLeft: 10}]}>{element.text}</Text>
+            </TouchableHighlight>
+          </ImageLabel>
+        );
+      } else {
+        renderedData.push(
+          <ImageLabel key={idx} source={element.source} style={IMAGE_STYLE}>
+            <Text style={[{color: TEXT_COLOR, textAlignVertical: 'center', paddingLeft: 10}]}>{element.text}</Text>
+          </ImageLabel>
+        );
+      }
     });
 
     return (
