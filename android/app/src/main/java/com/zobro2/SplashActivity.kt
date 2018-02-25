6f6bd7620e622486675800f16f2e697fc0edf7ab
Robert Duriancik <r.duriancik@gmail.com>
2018-02-25 17:48:57 +0100

    [fix] Fix Android version, convert Java files to Kotlin

---
+++ android/app/src/main/java/com/zobro2/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.zobro3
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by duriancik on 24/02/18.
+ */
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
